87731521aa1ccb1eb11ba7404446c8b1de14a61d
fauguste <frederic.auguste@gmail.com>
2018-11-23 23:15:22 +0100

    Add travis badge

--- Readme.md
+++ Readme.md
@@ -1,3 +1,9 @@
 # GetHue Docker
 
-Weekly published Docker of gethue http://gethue.com/.
+[![Build Status](https://travis-ci.org/fauguste/gethue.svg?branch=master)](https://travis-ci.org/fauguste/gethue)
+
+
+Weekly published Docker of [gethue](http://gethue.com/).
+
+
+[Docker](https://hub.docker.com/r/fauguste/gethue/)
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 today=$(date +%d%M%y)
 
 docker build -f Dockerfile -t gethue .
-docker tag gethue:latest fauguste/cofrac_web:latest
-docker push fauguste/cofrac_web:latest
-docker tag gethue:$today fauguste/cofrac_web:$today
-docker push fauguste/cofrac_web:$today
+docker tag gethue:latest fauguste/gethue:latest
+docker push fauguste/gethue:latest
+docker tag gethue:latest fauguste/gethue:$today
+docker push fauguste/gethue:$today
