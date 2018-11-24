5280beb193e6afb04cc5e8a945fefe72165e40b5
fauguste <frederic.auguste@gmail.com>
2018-11-24 10:36:57 +0100

    debug

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
 docker build -f Dockerfile -t gethue .
 docker tag gethue:latest fauguste/gethue:latest
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push fauguste/gethue:latest
