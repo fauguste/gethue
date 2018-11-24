7392b16afe78837a88b1fec6a24255baddf40e41
fauguste <frederic.auguste@gmail.com>
2018-11-24 09:48:19 +0100

    remove weekly tag

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-today=$(date +%d%M%y)
 
 docker build -f Dockerfile -t gethue .
 docker tag gethue:latest fauguste/gethue:latest
 docker push fauguste/gethue:latest
-docker tag gethue:latest fauguste/gethue:$today
-docker push fauguste/gethue:$today
